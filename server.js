073b970e8fef76cb05c8a24de79648b47a6ac489
joseffikitti <joseffikitti@gmail.com>
2026-03-02 09:12:18 +0100

    tanolyamok oldalon profil jo

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.get("/tanfolyamok/:slug", (req, res) => {
 // ===== ADATBÁZIS =====
 const db = mysql.createConnection({
   host: "localhost",
-  port: 3306,
+  port: 3307,
   user: "root",
   password: '',
   database: "mestermunka"
